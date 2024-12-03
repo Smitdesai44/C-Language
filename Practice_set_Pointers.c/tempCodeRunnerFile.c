31aa772856bd26ec474ab3852ac536c49fb594c1
Smitdesai44 <smitdesai204@gmail.com>
2024-12-03 11:01:11 +0530

    first commit

---
+++ Practice_set_Pointers.c/tempCodeRunnerFile.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    for(int i = 1 ; i <= 25 ; i++)
+    {
+        // n = i * i;
+        printf("\n%d = %d", i ,i*i);
+        // printf("\n%d", n);
+    }
+}
